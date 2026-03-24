0e61cfd2881b0411b347dd8ec8d0e6112416f934
iawnix <iawhaha@163.com>
2026-03-24 22:29:45 +0800

    fix by m2

--- src/tool/mol_tool.py
+++ src/tool/mol_tool.py
@@ -1,6 +1,6 @@
 import rdkit
 from rdkit import Chem
-from rdkit.Chem import AllChem, Draw, rdmolops, DataStructs, rdFingerprintGenerator, rdDepictor, Descriptors
+from rdkit.Chem import AllChem, SDWriter, Draw, rdmolops, DataStructs, rdFingerprintGenerator, rdDepictor, Descriptors
 from rdkit.Chem.rdchem import Mol
 from rdkit.DataStructs.cDataStructs import ExplicitBitVect
 from rdkit.ML.Descriptors import MoleculeDescriptors
@@ -16,6 +16,7 @@ project_root = Path(__file__).parent.parent
 sys.path.append(str(project_root))
 from util.constants import METAL_TYPE, COORD_TYPE
 
+from copy import deepcopy
 
 #+++++++++++++++++++++++++++++++++++++++# READ ME #++++++++++++++++++++++++++++++++++++++++++++++++#
 # add_dative
@@ -30,6 +31,8 @@ def add_dative(mol: Mol, meta_idx: int, coord_idx: List) -> Mol:
     for i in coord_idx:
         if rw_mol.GetBondBetweenAtoms(i, meta_idx) is None:
             rw_mol.AddBond(i, meta_idx, order=Chem.BondType.DATIVE)
+        else:
+            print("Warning[iaw]:> Please remove the bond between atom[{}] and atom[{}]!".format(i, meta_idx))
     new_mol = rw_mol.GetMol()
     return new_mol
 
@@ -130,9 +133,11 @@ def gen_3D(smi: str, mol: Union[bool, Mol] = False) -> Mol:
 def check_atom_charge(mol: Mol, atom_idx: List[int]) -> None:
     for idx in atom_idx:
         atom = mol.GetAtomWithIdx(idx)
-        print("Info[iaw]>: ATOM[{}], Charge: {}, Valence: {}".format(atom.GetSymbol()
+        print("Info[iaw]>: ATOM[{}-{}], Charge: {}, Valence: {}, RadicalElectron: {}".format(atom.GetSymbol()
+                                                                     , idx
                                                                      , atom.GetFormalCharge()
-                                                                     , atom.GetTotalValence()))
+                                                                     , atom.GetTotalValence()
+                                                                     , atom.GetNumRadicalElectrons()))
 
 
 def correct_valence(mol: Mol, infos: Tuple[int, int, int]) -> Mol:
@@ -147,5 +152,76 @@ def correct_valence(mol: Mol, infos: Tuple[int, int, int]) -> Mol:
 
 def split_cat_smi() -> Tuple[str, str]:
     pass
+
+
+def repair_mol2(mol: Mol, ion_coor_s: List[Tuple[int, List[int]]]
+                    , valence_info_s: List[Tuple[int, int, int]], print_out: bool = False) -> Union[Tuple[str, Mol], Tuple[None, None]]:
+    """
+    - `ion_coor_s` format: `[(int_1, (int2, ...)), ....]`
+        - `(int_1, (int2, ...))`: group1
+        - `int_1`: ion atom in group1
+        - `(int2, ...)`: coordinating atoms in group1
+    - `valence_info_s` format: `[(int_1, int_2, int_3), ...]`
+        - `(int_1, int_2, int_3)`: atom1
+        - `int_1`: index (0-based) of atom1
+        - `int_2`: NumRadicalElectrons of atom1
+        - `int_3`: FormalCharge of atom1
+    """
+    mol_fixed = deepcopy(mol)
+    try:
+        # repair 1
+        for i_ion_coor in ion_coor_s:
+            mol_fixed = add_dative(mol_fixed, i_ion_coor[0], i_ion_coor[1])
+            mol_fixed.UpdatePropertyCache(strict=False)
+    
+        # repair 2
+        for i_info in valence_info_s:
+            mol_fixed = correct_valence(mol_fixed, i_info)
+            mol_fixed.UpdatePropertyCache(strict=False)
+
+        smi_fixed = Chem.MolToSmiles(mol_fixed, isomericSmiles=True)
+    except:
+        if print_out:
+            print("Error[iaw]:> can not repair mol2")
+        smi_fixed = None
+        mol_fixed = None
+    
+    return (smi_fixed, mol_fixed)
+   
+
+def repair_mol2_and_save_as_sdf(mol_name: str, mol2_fp: str, sdf_saved_fp: str
+                        , ion_coor_s: List[Tuple[int, List[int]]]
+                        , valence_info_s: List[Tuple[int, int, int]]) -> Union[Tuple[str, Mol], Tuple[None, None]]:
+    """
+    - `ion_coor_s` format: `[(int_1, (int2, ...)), ....]`
+        - `(int_1, (int2, ...))`: group1
+        - `int_1`: ion atom in group1
+        - `(int2, ...)`: coordinating atoms in group1
+    - `valence_info_s` format: `[(int_1, int_2, int_3), ...]`
+        - `(int_1, int_2, int_3)`: atom1
+        - `int_1`: index (0-based) of atom1
+        - `int_2`: NumRadicalElectrons of atom1
+        - `int_3`: FormalCharge of atom1
+    """
+    try:
+        # read mol2
+        mol = Chem.MolFromMol2File(mol2_fp, removeHs = False)
+    except:
+        print("Error[iaw]:> can read mol from `{}`".format(mol2_fp))
+    
+
+    smi_fixed, mol_fixed = repair_mol2(mol, ion_coor_s, valence_info_s, print_out = False)
+    if smi_fixed == None or mol_fixed == None:
+        print("Error[iaw]:> can not repair mol2 `{}`".format(mol_name))
+    else:
+        with SDWriter(sdf_saved_fp) as writer:
+            mol_fixed.SetProp("_Name", mol_name)
+            mol_fixed.SetProp("SMILES", smi_fixed)
+            writer.write(mol_fixed)
+
+    return smi_fixed, mol_fixed
+
+
+
 
 
