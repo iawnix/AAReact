93c4dbfb8a3923ea502f8347a785338b57b17cd6
iawnix <iawhaha@163.com>
2026-05-14 20:50:58 +0800

    add dataset by iaw

---
+++ DataSet/Data_All/1_process.sh
@@ -0,0 +1,8 @@
+
+
+python /home/iaw/DATA2/AAReact/src/AHO_calc_raw_features.py \
+    --in_csv "/home/iaw/DATA2/AAReact/DataSet/Data_All/full_data_464.csv" \
+    --desc_type "all" \
+    --sdf_path "/home/iaw/DATA2/AAReact/DataSet/Data_All/1_sdf" \
+    --save_path "/home/iaw/DATA2/AAReact/DataSet/Data_All/2_raw_features"
+
