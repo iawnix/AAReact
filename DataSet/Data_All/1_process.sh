fc9806ab581a3a63cef11391582b010709bb3d97
iawnix <iawhaha@163.com>
2026-06-29 20:38:58 +0800

    Last update for pred ee by iaw

--- DataSet/Data_All/1_process.sh
+++ DataSet/Data_All/1_process.sh
@@ -1,8 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
+PYTHON="/home/iaw/soft/conda/2024.06.1/envs/python3.12/bin/python"
+SAVE_PATH="/home/iaw/DATA2/AAReact/DataSet/Data_All/2_raw_features"
 
-python /home/iaw/DATA2/AAReact/src/AHO_calc_raw_features.py \
-    --in_csv "/home/iaw/DATA2/AAReact/DataSet/Data_All/full_data_464.csv" \
+mkdir -p "${SAVE_PATH}"
+
+"${PYTHON}" /home/iaw/DATA2/AAReact/src/AHO_calc_raw_features.py \
+    --in_csv "/home/iaw/DATA2/AAReact/DataSet/Data_All/full_data_436-20260624.csv" \
     --desc_type "all" \
     --sdf_path "/home/iaw/DATA2/AAReact/DataSet/Data_All/1_sdf" \
-    --save_path "/home/iaw/DATA2/AAReact/DataSet/Data_All/2_raw_features"
-
+    --save_path "${SAVE_PATH}"
